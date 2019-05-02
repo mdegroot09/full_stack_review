f6f7be2d1fb445b0a58a3f9c4d46c7e232b9d2ff
Mike De Groot <mdegroot09@gmail.com>
2019-05-02 10:16:04 -0600

    set up server with sessions, database

--- server/index.js
+++ server/index.js
@@ -3,5 +3,21 @@ const app = express()
 require('dotenv').config()
 const massive = require('massive')
 const session = require('express-session')
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
 
 app.use(express.json())
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 //resets after 24 hours
+  }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+  app.listen(SERVER_PORT, () => {
+    console.log('listening on port:', SERVER_PORT)
+  })
+})
